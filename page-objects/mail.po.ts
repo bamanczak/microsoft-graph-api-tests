ba762840ee325099ed47c5a560fdfc787841715e
bamanczak <bamanczak@gmail.com>
2020-07-15 12:58:47 +0200

    Added verification of attachments content

--- page-objects/mail.po.ts
+++ page-objects/mail.po.ts
@@ -1,27 +1,31 @@
 import * as MicrosoftGraph from "@microsoft/microsoft-graph-types"
+import "isomorphic-fetch";
+
+const domain: string = "https://graph.microsoft.com/v1.0/"
 
 
 export class MailApi {
-    sendMail (accessToken: string, mail) {
-        let url = "https://graph.microsoft.com/v1.0/me/sendMail";
+    sendEmail(accessToken: string, mail) {
+        let url = domain + "me/sendMail";
         let request = new Request(
             url, {
-                method: "POST",
-                body: JSON.stringify({
-                    message: mail
-                }),
-                headers: new Headers({
-                    "Authorization": "Bearer " + accessToken,
-                    'Content-Type': 'application/json'
-                })
-            }
+            method: "POST",
+            body: JSON.stringify({
+                message: mail
+            }),
+            headers: new Headers({
+                "Authorization": "Bearer " + accessToken,
+                'Content-Type': 'application/json'
+            })
+        }
         );
+
         return fetch(request);
     }
 
-    readMail (accessToken: string, subject: string) {
-        let received = "https://graph.microsoft.com/v1.0/me/messages";
-        let readMailRequest = new Request(received, {
+    getEmail(accessToken: string) {
+        let url = domain + "me/messages";
+        let readMailRequest = new Request(url, {
             method: "GET",
             headers: new Headers({
                 "Authorization": "Bearer " + accessToken
@@ -29,5 +33,28 @@ export class MailApi {
         });
 
         return fetch(readMailRequest);
+    }
+
+    getLatestEmail(accessToken: string) {
+        return this.getEmail(accessToken)
+            .then((response) => response.json())
+            .then((res) => {
+                let messages: [MicrosoftGraph.Message] = res.value;
+                return messages[0];
+            });
+    }
+
+    getAttachments(accessToken: string, mailId: string) {
+        let url = domain + "me/messages/" + mailId + "/attachments";
+        let getAttachmentsRequest = new Request(url, {
+            method: "GET",
+            headers: new Headers({
+                "Authorization": "Bearer " + accessToken
+            })
+        });
+
+        return fetch(getAttachmentsRequest)
+            .then((response) => response.json())
+            .then((attachments) => attachments.value);
     }
 }
